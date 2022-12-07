17568b29f040fcb411d250fb9f2b241a84404e1a
PsykeDady <psdady@msn.com>
2022-12-07 17:33:30 +0100

    dev next function

--- LittleJSonH.js
+++ LittleJSonH.js
@@ -3,7 +3,8 @@ const cronExprOrdinal = {
 	HOURS: 1,
 	DAY_OF_MONTH: 2,
 	MONTH: 3,
-	DAY_OF_WEEK: 4
+	DAY_OF_WEEK: 4,
+	YEAR:5
 }
 
 const ERR_TEXT_WRONG_CRON = "ERROR= can't parse this string"
@@ -15,13 +16,35 @@ const WORDLIST_DAY_OF_WEEK = "SUNMONTUEWEDTHUFRISAT";
 
 const MAX_YEAR_TRY = 4;
 
+/**
+	* 
+	* @param {number} month 
+	* @param {number} year 
+	* @returns 
+	*/
+function monthLength(month, year) {
+		
+	switch (month) {
+		case 0:case 2:case 4:case 6:case 7:case 9:case 11: return 31;
+		case 3:case 5:case 8:case 10: return 30;
+	}
+	
+	return  year%4==0 && (year%400==0 || year%100!=0 )  ? 29 : 28;
+}
+
+
 class LittleJSonH {
 	_cronexpr = "";
-	_minutes = [] // 60
-	_hours = [] // 24
-	_daysOfMonth = [] // 31
-	_month = [] // 12
-	_daysOfWeek = [] // 7
+	/** @type{boolean[]} */
+	_minutes = new Array(60).map(v=>false); // 60
+	/** @type{boolean[]} */
+	_hours = new Array(24).map(v=>false); // 24
+	/** @type{boolean[]} */
+	_daysOfMonth = new Array(31).map(v=>false); // 31
+	/** @type{boolean[]} */
+	_months = new Array(12).map(v=>false); // 12
+	/** @type{boolean[]} */
+	_daysOfWeek = new Array(7).map(v=>false); // 7
 
 	currentTime = new Date()
 	human = ""
@@ -53,7 +76,7 @@ class LittleJSonH {
 					output = this._daysOfMonth;
 					break;
 				case cronExprOrdinal.MONTH:
-					output = this._month;
+					output = this._months;
 					break;
 				case cronExprOrdinal.DAY_OF_WEEK:
 					output = this._daysOfWeek;
@@ -63,6 +86,7 @@ class LittleJSonH {
 			}
 			let hword=this._analyze(splitCron[i], output, CRON_EXPR_ORDER[i])
 			this.human=(this.human=="")?hword: this.human+", "+hword;
+			this.currentTime=new Date();
 		}
 	}
 
@@ -84,6 +108,107 @@ class LittleJSonH {
 		if (mod != 0) throw ERR_TEXT_WRONG_CRON;
 
 		return ind / 3;
+	}
+
+	/**
+		* 
+		* @param {boolean[]} values 
+		* @param {number} index 
+		* @returns 
+		*/
+	_searchNextOccurence(values, index) {
+		if(index>values.length) index=0; // restart
+		for (let tmp=index+1;tmp<values.length;tmp++) {
+			if(values[tmp]) return tmp;
+		}
+		for (let tmp=0;tmp<index;tmp++) {
+			if(values[tmp]) return tmp;
+		}
+		return index;
+	}
+	
+	/**
+		* 
+		* @param {int[]} time 
+		* @returns 
+		*/
+	_nextMinute(time) {
+		let tmp=0;
+		tmp=this._searchNextOccurence(this._minutes, time[cronExprOrdinal.MINUTES]); //update minute
+		time[cronExprOrdinal.HOURS]=tmp<=time[cronExprOrdinal.MINUTES]?time[cronExprOrdinal.HOURS]+1:time[cronExprOrdinal.HOURS]; // update hour
+		time[cronExprOrdinal.MINUTES]=tmp; // update minute
+		
+		return time;
+	}
+
+	_nextHour(time) {
+		let tmp=0;
+		tmp=this._searchNextOccurence(this._hours, time[cronExprOrdinal.HOURS]); //update Hour
+		time[cronExprOrdinal.DAY_OF_MONTH]=tmp<=time[cronExprOrdinal.HOURS]?time[cronExprOrdinal.DAY_OF_MONTH]+1:time[cronExprOrdinal.DAY_OF_MONTH]; // update dM
+		time[cronExprOrdinal.HOURS]=tmp; // update Hour
+		time[cronExprOrdinal.MINUTES]=0;
+		
+		return time;
+	}
+	
+	_nextDayOfMonth(time) {
+		let tmp=0;
+		
+		tmp=this._searchNextOccurence(this._daysOfMonth, time[cronExprOrdinal.DAY_OF_MONTH]); //update dM
+		time[cronExprOrdinal.MONTH]=tmp<=time[cronExprOrdinal.DAY_OF_MONTH]?time[cronExprOrdinal.MONTH]+1:time[cronExprOrdinal.MONTH]; // update month
+		time[cronExprOrdinal.DAY_OF_MONTH]=tmp; // update dM
+		time[cronExprOrdinal.HOURS]=time[cronExprOrdinal.MINUTES]=0;
+		
+		return time;
+	}
+	
+	_nextMonth(time) {
+		let tmp=0;
+		
+		tmp=this._searchNextOccurence(this._months, time[cronExprOrdinal.MONTH]);
+		time[cronExprOrdinal.YEAR]=tmp<=time[cronExprOrdinal.MONTH]?time[cronExprOrdinal.YEAR]+1:time[cronExprOrdinal.YEAR];
+		time[cronExprOrdinal.MONTH]=tmp;
+		time[cronExprOrdinal.DAY_OF_MONTH]=time[cronExprOrdinal.HOURS]=time[cronExprOrdinal.MINUTES]=0;
+		
+		return time;
+	}
+
+	nextT() {
+		let minute=this.currentTime.getMinutes()
+		let hours=this.currentTime.getHours()
+		let dayOfMonth=this.currentTime.getDate()
+		let month=this.currentTime.getMonth()
+		let dayOfWeek=this.currentTime.getDay()
+		let year=this.currentTime.getFullYear()
+		
+		let time = [minute,hours,dayOfMonth,month,dayOfWeek,year]
+		console.log(time[0],'|',time[1],'|',time[2],'|',time[3] + 1,'|',time[4],'|',time[5])
+		
+		
+		this._nextMinute(time)
+		
+		let repeatitions = MAX_YEAR_TRY; 
+
+		while (repeatitions > -1 ){
+			if( time[cronExprOrdinal.MONTH]>=this._months.length || ! this._months[time[cronExprOrdinal.MONTH]]) {
+				year=time[cronExprOrdinal.YEAR];
+				this._nextMonth(time);
+				if ( year!==time[cronExprOrdinal.YEAR]) repeatitions--;
+			} else if (time[cronExprOrdinal.DAY_OF_MONTH]>=monthLength(time[cronExprOrdinal.MONTH], time[cronExprOrdinal.YEAR]) || ! this._daysOfMonth[time[cronExprOrdinal.DAY_OF_MONTH]]) { 
+				this._nextDayOfMonth(time);
+			} else if (time[cronExprOrdinal.HOURS]>=this._hours.length || ! this._hours[time[cronExprOrdinal.HOURS]]) {
+				this._nextHour(time);
+			} else if (time[cronExprOrdinal.MINUTES]>=this._minutes.length || ! this._minutes[time[cronExprOrdinal.MINUTES]]) {
+				this._nextMinute(time);
+			} else {
+				this.currentTime=new Date(time[cronExprOrdinal.YEAR],time[cronExprOrdinal.MONTH], time[cronExprOrdinal.DAY_OF_MONTH], time[cronExprOrdinal.HOURS], time[cronExprOrdinal.MINUTES]);
+				time[cronExprOrdinal.DAY_OF_WEEK]=this.currentTime.getDay();
+				if( this._daysOfWeek[time[cronExprOrdinal.DAY_OF_WEEK]] ) break;
+				this._nextDayOfMonth(time);
+			}
+		}
+		console.log(time[0],'|',time[1],'|',time[2],'|',time[3] + 1,'|',time[4],'|',time[5])
+		
 	}
 
 	/**
@@ -217,6 +342,8 @@ class LittleJSonH {
 	}
 }
 
+console.log("_______________")
+
 let test="* * * * *"
 let l= new LittleJSonH(test); 
 console.log(test+" : "+l.human)
@@ -257,6 +384,22 @@ test="* * */14 * *"
 l= new LittleJSonH(test); 
 console.log(test+" : "+l.human)
 
+console.log("_______________\ntest nextT()")
+test="* * * * *"
+l= new LittleJSonH(test);
+console.log(test+" : "+l.human)
+console.log(l.currentTime)
+l.nextT()
+console.log(l.currentTime)
+
+test="30 2 10 MAR *"
+l= new LittleJSonH(test);
+console.log(test+" : "+l.human)
+console.log(l.currentTime)
+l.nextT()
+console.log(l.currentTime)
 
 
+
+console.log("_______________")
 console.log("the end")
